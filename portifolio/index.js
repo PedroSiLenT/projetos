919628c2214c3d78e14ae8c0885d91e58c44bc15
Pedro Henrique <140028695+PedroSiLenT@users.noreply.github.com>
2024-07-02 16:53:32 -0300

    att

---
+++ portifolio/index.js
@@ -0,0 +1,18 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
